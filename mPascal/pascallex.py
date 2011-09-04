c1963a923d320590a2a21fc8b41e8eb5db31b985
Andrés Torres <elfotografo007@gmail.com>
2011-09-04 13:26:59 -0500

    Se corrige problema entre flotantes y enteros. Se agrega comprobación de 0 a la izquierda en flotantes

--- mPascal/pascallex.py
+++ mPascal/pascallex.py
@@ -24,9 +24,10 @@ reserved = { 'while' : 'WHILE',
             'and' : 'AND', 
             'or' : 'OR', 
             'not' : 'NOT'}
-tokens = list(reserved.values()) + ['IDENTIFICADOR', 'ENTERO', 'FLOTANTE', 'COMENTARIO', 'CADENA', 'ASIGNACION', 'IGUALIGUAL', 'MENORIGUAL', 'MAYORIGUAL', 'DIFERENTE']
+tokens = list(reserved.values()) + ['IDENTIFICADOR',  'FLOTANTE', 'ENTERO', 'COMENTARIO', 'CADENA', 'ASIGNACION', 'IGUALIGUAL', 'MENORIGUAL', 'MAYORIGUAL', 'DIFERENTE']
 
 literals = ['(', ')', '[', ']', '+', '-', '*', '/', ':', ';', '<', '>', ',']
+
 
 t_ignore = " \t"
 
@@ -34,18 +35,21 @@ def t_ignore_COMENTARIO(t):
     r"/\*(.|\n)*?\*/"
     t.lexer.lineno += t.value.count("\n")
 
-def t_IDENTIFICADOR(t):
-    r"[_a-zA-Z][_a-zA-Z\d]*"
-    #Verifica que no sea una palabra reservada. Si t.value esta en reserved lo asigna, si no, lo deja como identificador
-    t.type = reserved.get(t.value, 'IDENTIFICADOR') 
+def t_FLOTANTE(t):
+    r"(\d+\.\d+)([e][+-]?\d+)?| \d+[e][+-]?\d+"
+    if (t.value[0] == '0' and t.value[1] != '.' and len(t.value) > 1):
+        print "Flotante ilegal: ", t.value
+        t.value = 0
+        t.lexer.skip(1)
+    #try:
+     #   t.value = float(t.value)
+    #except ValueError:
+     #   print "Flotante demasiado largo", t.value
+      #  t.value = 0
     return t
 
-t_CADENA = r"\"([^\\\n]|(\\.))*\""
-
-t_FLOTANTE = r"(\d+\.\d+)([e][+-]?\d+)?| \d+[e][+-]?\d+"
-
 def t_ENTERO(t):
-    r"\d+(?!\d*\.\d+)"
+    r"\d+"
     if (t.value[0] == '0' and len(t.value) > 1):
         print "Entero ilegal: ", t.value
         t.lexer.skip(1)
@@ -55,6 +59,7 @@ def t_ENTERO(t):
         print "Entero demasiado largo", t.value
         t.value = 0
     return t
+
 
 t_ASIGNACION = r":="
 
@@ -66,6 +71,14 @@ t_MAYORIGUAL = r">="
 
 t_DIFERENTE = r"!="
 
+t_CADENA = r"\"([^\\\n]|(\\.))*\""
+
+
+def t_IDENTIFICADOR(t):
+    r"[_a-zA-Z][_a-zA-Z\d]*"
+    #Verifica que no sea una palabra reservada. Si t.value esta en reserved lo asigna, si no, lo deja como identificador
+    t.type = reserved.get(t.value, 'IDENTIFICADOR') 
+    return t
 
 
 def t_newline(t):
