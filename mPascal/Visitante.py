41351f837a003aad932296ca8d66f73c49cfe33b
Andrés Torres <elfotografo007@gmail.com>
2012-02-10 10:45:03 -0500

    Se crea la clase visitante

---
+++ mPascal/Visitante.py
@@ -0,0 +1,16 @@
+'''
+Created on 10/02/2012
+
+@author: elfotografo007
+'''
+
+class Visitante(object):
+    '''
+    Clase Visitante
+    '''
+    def visiteme(self, objeto):
+        pass
+
+    
+        
+        
