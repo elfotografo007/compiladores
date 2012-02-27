dfb3cd54dd1f8030d31126a9272ecb5b82e38993
Andrés Torres <elfotografo007@gmail.com>
2012-02-27 13:25:38 -0500

    Se crea archivo mpasgen.py

---
+++ mPascal/mpasgen.py
@@ -0,0 +1,8 @@
+'''
+Created on 27/02/2012
+
+@author: elfotografo007
+'''
+def generate(file, top):
+    print >>file, "!Creado por mpascal.py"
+    print >>file, "! Esteban Santa y Andres Torres, IS744 (2012-1)"
